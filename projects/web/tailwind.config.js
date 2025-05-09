2939918a707cbc240828c382a85b658d5250ad5d
zinny22 <hziny722@naver.com>
2025-05-09 22:45:49 +0900

    feat: init project

---
+++ projects/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../design-system/src/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
