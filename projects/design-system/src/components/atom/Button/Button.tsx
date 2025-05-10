59689002314c62797bda2fdc4b1d886efdc17ef6
zinny22 <hziny722@naver.com>
2025-05-10 19:55:05 +0900

    feat: 버튼 컴포넌트 제작

--- projects/design-system/src/components/atom/Button/Button.tsx
+++ projects/design-system/src/components/atom/Button/Button.tsx
@@ -1,25 +1,124 @@
-import React from "react";
+import React, { ComponentProps, ReactNode } from "react";
+import { ColorType, Size, State, Variant } from "@/types/type";
+import { cva, VariantProps } from "class-variance-authority";
 
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: "small" | "medium" | "large";
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
-  onClick?: () => void;
-}
+export type ButtonVariantProps = VariantProps<typeof buttonVariants>;
 
-/** Primary UI component for user interaction */
+export type ButtonProps = ComponentProps<"button"> &
+  ButtonVariantProps & {
+    color?: ColorType;
+    variant?: Variant;
+    state?: State;
+    size?: Size;
+    fullWidth?: boolean;
+    children: ReactNode;
+  };
+
+export const buttonVariants = cva(
+  "flex items-center justify-center gap-x-1 rounded-[5px]",
+  {
+    variants: {
+      fullWidth: {
+        true: "w-full",
+        false: "",
+      },
+      color: {
+        primary: "bg-orange-900 text-white",
+        secondary: "bg-white text-gray-700 border border-gray-300",
+        tertiary: "bg-gray-900 text-white",
+        cta: "bg-cta text-white",
+      },
+      variant: {
+        container: "",
+        outline: "",
+        ghost: "",
+        text: "",
+      },
+      state: {
+        enable: "",
+        disable: "!bg-gray-500 text-white",
+        skeleton: "!bg-gray-200 !text-gray-200",
+      },
+      size: {
+        XS: "text-btn12Bd p-2 h-[30px]",
+        S: "text-btn14Sm py-2 px-[10px] h-[36px]",
+        M: "text-btn16Sm py-[13px] px-[14px] h-[48px]",
+        L: "text-btn18Sm py-4 px-4 h-[52px]",
+        XL: "text-btn20Sm py-4 px-4 h-[56px]",
+      },
+    },
+    defaultVariants: {
+      color: "primary",
+      variant: "container",
+      size: "S",
+    },
+
+    compoundVariants: [
+      {
+        color: "primary",
+        variant: "outline",
+        state: "enable",
+        class: "bg-white text-orange-900 border border-orange-900",
+      },
+      {
+        color: "primary",
+        variant: "ghost",
+        class: "bg-white text-orange-900 ",
+      },
+      {
+        color: "secondary",
+        state: "disable",
+        class: "!bg-white border border-gray-300 text-gray-300",
+      },
+      {
+        color: "secondary",
+        variant: "ghost",
+        class: "!bg-white text-gray-700 border border-white",
+      },
+      {
+        color: "tertiary",
+        variant: "outline",
+        state: "enable",
+        class: "!bg-white text-gray-700 border border-gray-700",
+      },
+      {
+        color: "tertiary",
+        variant: "ghost",
+        state: "enable",
+        class: "!bg-white text-gray-900 ",
+      },
+      {
+        color: "cta",
+        variant: "outline",
+        state: "enable",
+        class: "!bg-white border border-cta text-cta",
+      },
+      {
+        color: "cta",
+        variant: "ghost",
+        state: "enable",
+        class: "!bg-white text-cta",
+      },
+    ],
+  }
+);
+
 export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
-  label,
+  color = "primary",
+  variant = "container",
+  size = "M",
+  fullWidth = false,
+  children,
+  state = "enable",
   ...props
 }: ButtonProps) => {
-  return <button className="p-5 text-orange-900">가나다라?</button>;
+  return (
+    <button
+      className={buttonVariants({ color, variant, size, state, fullWidth })}
+      disabled={state !== "enable"}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
---
+++ projects/design-system/src/types/type.ts
@@ -0,0 +1,7 @@
+export type ColorType = "primary" | "secondary" | "tertiary" | "cta";
+
+export type Variant = "container" | "outline" | "ghost";
+
+export type State = "enable" | "disable" | "skeleton";
+
+export type Size = "XS" | "S" | "M" | "L" | "XL";
